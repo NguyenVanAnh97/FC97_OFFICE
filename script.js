1ad6d57b8ac79b6f6adf04ce76344dfa8cd45881
vace <nguyenvananh270897@gmail.com>
2023-09-22 12:49:59 +0700

    update something

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const nav = document.querySelector(".nav"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
 
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
