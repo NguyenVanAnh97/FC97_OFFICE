fe502031ad285996ffdadfabefd16c41caac6c37
vace <nguyenvananh270897@gmail.com>
2023-10-05 22:52:42 +0700

    change

--- script.js
+++ script.js
@@ -10,32 +10,6 @@ navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
 
-// Đặt ngày sinh nhật của bạn ở đây (năm, tháng, ngày)
-const birthday = new Date('2023-10-02');
-        
-function updateCountdown() {
-    const now = new Date();
-    const timeLeft = birthday - now;
-
-    if (timeLeft <= 0) {
-        document.getElementById('countdown').innerHTML = 'Chúc mừng sinh nhật!';
-    } else {
-        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-        document.getElementById('countdown').innerHTML = `(${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây)`;
-    }
-}
-
-function updateBirthday() {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-}
-
-// Cập nhật thời gian đếm ngược và ngày sinh nhật ban đầu
-updateCountdown();
-updateBirthday();
 
 // Cập nhật thời gian đếm ngược mỗi giây
 setInterval(updateCountdown, 1000);
